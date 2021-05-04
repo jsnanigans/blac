eeba778d2a69db7d435255d2523e2d4aba4fc9a1
Brendan Mullins <megamaaaan@gmail.com>
2021-05-04 12:26:26 +0200

    Make .emit public for Cubit

--- src/lib/Cubit.ts
+++ src/lib/Cubit.ts
@@ -1,7 +1,7 @@
 import BlocBase from "./BlocBase";
 
 export default class Cubit<T> extends BlocBase<T> {
-  protected emit = (value: T): void => {
+  public emit = (value: T): void => {
     this.notifyChange(value);
     this.next(value);
   };
