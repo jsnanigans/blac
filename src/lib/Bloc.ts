2f456395515e17c8498d52e7653241e0575a0a02
gokturkDev <gokturkbuyuktuna00@gmail.com>
2022-04-14 14:58:02 +0300

    bug#2, events can contain data or be instantiated
    P.S: This took waaaay longer than it should, its definitely a hacky solution but works for now

--- src/lib/Bloc.ts
+++ src/lib/Bloc.ts
@@ -24,14 +24,50 @@ export default class Bloc<E, T> extends BlocBase<T> {
 
   public add = (event: E): void => {
     for (const [eventName, handler] of this.eventHandlers) {
-      if (eventName == event) {
-        handler(event, this.emit(event), this.state);
-        return;
+      if(this.isEventPassedCorrespondTo(event, eventName, handler)){
+        handler(event, this.emit(event), this.state)
+        return
       }
     }
-
     console.warn(`Event is not handled in Bloc:`, { event, bloc: this });
   };
+
+  private isEventPassedCorrespondTo = (passedEvent: E, registeredEventName: E, registeredEventHandler: EventHandler<E, T>) =>{
+
+      return this.didAddNonInstantiatedEvent(passedEvent, registeredEventName, registeredEventHandler) ||
+      this.didAddInstantiatedEvent(passedEvent, registeredEventName, registeredEventHandler);
+
+  }
+
+  private didAddNonInstantiatedEvent(event: E, eventName: E, handler: EventHandler<E, T>){
+    return eventName === event;
+  }
+
+
+  private didAddInstantiatedEvent(eventAsObject: E, eventAsFunction: E, registeredHandler: EventHandler<E, T>){
+    /*
+      A very hacky solution. JS is a nightmare with objects.
+      Normally we check the events as the same type or not.
+      However sometimes client needs to pass in data with the event, in that circumstance,
+      they need to have the payload in the event, meaning they instantiate the event.
+      That makes the type and event different, even more so
+      since the type is abstract and event is a instantiated subclass
+      thanks to the grand js, we litterally cannot check if one is another or cast (generic types) or
+      type-check. (i couldn't find a better solution btw maybe we can)
+
+      Moreover the code stores instantiated events as lambda functions
+
+      Now, to check type and object equality, we need to get their real"Subclass"Names to compare them
+      As you can see from realEventName, we get the real class Name, then
+      we take the constructor name of the input event and since the constructor name will
+      equal to real name of class, voila!
+       */
+    let realEventName = (eventAsFunction as any).name;
+    var constructorName = Object.getPrototypeOf(eventAsObject).constructor.name;
+
+    return realEventName === constructorName;
+  }
+
 
   private emit = (event: E) => (newState: T) => {
     this.notifyChange(newState);
---
+++ src/lib/usecase-tests/login/src/models/password.ts
@@ -0,0 +1,6 @@
+export class Password{
+  constructor(
+    public value: string
+  ) {
+  }
+}
