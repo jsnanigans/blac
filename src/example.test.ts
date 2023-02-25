ace7031abc064b700e2f74d6bc8dd5770f8d7716
Brendan Mullins <brendan.mullins@9am.health>
2023-02-25 13:15:00 +0100

    setup new example

---
+++ src/example.test.ts
@@ -0,0 +1,125 @@
+import { describe, expect, test, vitest } from 'vitest';
+import { CounterActions, CounterBloc, CounterCubit, globalBlacInstance } from './example';
+
+describe('Global Blac', () => {
+  test('should be registered in blac', () => {
+    const counter = new CounterBloc({ blac: globalBlacInstance });
+    expect(globalBlacInstance.globalState[counter.name]).toBe(counter.state);
+  });
+
+})
+
+describe('CounterBloc', () => {
+  test('should have a state', () => {
+    const counter = new CounterBloc();
+    expect(counter.state).toBe(0)
+  })
+
+  test('should increment', () => {
+    const counter = new CounterBloc();
+    counter.emit(CounterActions.increment)
+    expect(counter.state).toBe(1)
+  });
+
+  test('should decrement', () => {
+    const counter = new CounterBloc();
+    counter.emit(CounterActions.decrement)
+    expect(counter.state).toBe(-1)
+  });
+
+  test('should throw error if unknown action is passed', () => {
+    const counter = new CounterBloc();
+    expect(() => counter.emit('unknown' as any)).toThrowError('unknown action')
+  })
+
+  describe('observable', () => {
+    test('should notify subscribers', () => {
+      const counter = new CounterBloc();
+      const spy = vitest.fn();
+      counter.onStateChange(spy);
+      counter.emit(CounterActions.increment);
+      expect(spy).toHaveBeenCalledWith(1, 0);
+    })
+
+    test('should notify all subscribers', () => {
+      const counter = new CounterBloc();
+      const spy1 = vitest.fn();
+      const spy2 = vitest.fn();
+      counter.onStateChange(spy1);
+      counter.onStateChange(spy2);
+      counter.emit(CounterActions.increment);
+      expect(spy1).toHaveBeenCalledWith(1, 0);
+      expect(spy2).toHaveBeenCalledWith(1, 0);
+    })
+
+    test('should not notify after unsubscribing', () => {
+      const counter = new CounterBloc();
+      const spy = vitest.fn();
+      const unsubscribe = counter.onStateChange(spy);
+      unsubscribe();
+      counter.emit(CounterActions.increment);
+      expect(spy).not.toHaveBeenCalled();
+    })
+  })
+})
+
+describe('CounterCubit', () => {
+  test('should have a state', () => {
+    const counter = new CounterCubit();
+    expect(counter.state).toBe(0)
+  })
+
+  test('should update state', () => {
+    const counter = new CounterCubit();
+    counter.emit(1)
+    expect(counter.state).toBe(1)
+  });
+
+  test('should increment with method', () => {
+    const counter = new CounterCubit();
+    counter.increment()
+    expect(counter.state).toBe(1)
+  });
+
+  describe('observable', () => {
+    test('should notify subscribers', () => {
+      const counter = new CounterCubit();
+      const spy = vitest.fn();
+      counter.onStateChange(spy);
+      counter.emit(1);
+      expect(spy).toHaveBeenCalledWith(1, 0);
+    })
+
+    test('should notify all subscribers', () => {
+      const counter = new CounterCubit();
+      const spy1 = vitest.fn();
+      const spy2 = vitest.fn();
+      counter.onStateChange(spy1);
+      counter.onStateChange(spy2);
+      counter.emit(1);
+      expect(spy1).toHaveBeenCalledWith(1, 0);
+      expect(spy2).toHaveBeenCalledWith(1, 0);
+    })
+
+    test('should not notify subscribers if state is not changed', () => {
+      const counter = new CounterCubit();
+      const spy = vitest.fn();
+      counter.onStateChange(spy);
+      counter.emit(0);
+      expect(spy).not.toHaveBeenCalled();
+      counter.emit(0);
+      expect(spy).not.toHaveBeenCalled();
+    })
+  
+    test('should not notify after unsubscribing', () => {
+      const counter = new CounterCubit();
+      const spy = vitest.fn();
+      const unsubscribe = counter.onStateChange(spy);
+      unsubscribe();
+      counter.emit(1);
+      expect(spy).not.toHaveBeenCalled();
+    })
+  })
+})
+
+
--- src/example.ts
+++ src/example.ts
@@ -1,45 +1,110 @@
-class Blac {}
+/**
+ * OBSERVABLE
+ */
+type Observer<S> = (oldState: S, newState: S) => void;
+export class Observable<S> {
+  private _observers: Observer<S>[] = [];
 
+  constructor() {}
 
-abstract class Bloc<S, A> {
-  private _state: S;
-
-  get state() {
-    return this._state;
+  subscribe(observer: Observer<S>) {
+    this._observers.push(observer);
   }
-  
-  constructor(initialState: S) {
-    this._state = initialState;
+
+  unsubscribe(observer: Observer<S>) {
+    this._observers = this._observers.filter((obs) => obs !== observer);
   }
 
-  abstract reducer(action: A, state: S): S;
-
-  emit(action: A) {
-    this._state = this.reducer(action, this._state);
+  notify(newState: S, oldState: S) {
+    this._observers.forEach((observer) => observer(newState, oldState));
   }
 }
 
+/**
+ * BLAC
+ */
+class Blac {
+  globalState: any = {};
 
-const state = new Blac();
+  constructor() {
+  }
+}
 
+export const globalBlacInstance = new Blac();
 
+/**
+ * BLOC BASE
+ */
 
-// Counter Bloc
+export abstract class BlocBase<S> {
+  public _state: S;
+  public observable: Observable<S>;
+  public blac?: Blac;
+
+  constructor(initialState: S, options?: BlocOptions) {
+    this.observable = new Observable();
+    this._state = initialState;
+
+    if (options?.blac) {
+      this.blac = options?.blac;
+      this.blac.globalState[this.name] = this.state;
+    }
+  }
+  
+  get state() {
+    return this._state;
+  }
+
+  get name() {
+    return this.constructor.name;
+  }
+  
+  onStateChange = (callback: (newState: S, oldState: S) => void): () => void => {
+    this.observable.subscribe(callback);
+    return () => this.observable.unsubscribe(callback);
+  }
+}
+
+/**
+ * BLOC
+ */
+interface BlocOptions {
+  blac?: Blac;
+}
+
+export abstract class Bloc<S, A> extends BlocBase<S> {
+  abstract reducer(action: A, state: S): S;
+
+  emit = (action: A): void => {
+    const newState = this.reducer(action, this.state);
+    this.observable.notify(newState, this.state);
+    this._state = newState;
+  }
+}
+
+/**
+ * COUNTER BLOC
+ */
 type CounterState = number;
 
-enum CounterActions {
+export enum CounterActions {
   increment = 'increment',
   decrement = 'decrement',
 }
 type CounterAction = CounterActions;
 
 
-class Counter extends Bloc<CounterState, CounterAction> {
-  constructor() {
-    super(0);
+export class CounterBloc extends Bloc<CounterState, CounterAction> {
+  constructor(options: BlocOptions = {}) {
+    super(0, options);
   }
 
   reducer(action: CounterAction, state: CounterState) {
+    const actions = Object.values(CounterActions);
+    if (!actions.includes(action)) {
+      throw new Error(`unknown action: ${action}`);
+    }
+
     switch (action) {
       case CounterActions.increment:
         return state + 1;
@@ -49,11 +114,34 @@ class Counter extends Bloc<CounterState, CounterAction> {
   }
 }
 
-const counter = new Counter();
-// add to counter
-counter.emit(CounterActions.increment);
-// decrement counter
-counter.emit(CounterActions.decrement);
 
+/**
+ * CUBIT
+ */
+interface CubitOptions {
+  blac?: Blac;
+}
 
-// state.add(new )
+export abstract class Cubit<S> extends BlocBase<S> {
+  emit(state: S) {
+    if (state === this.state) {
+      return;
+    };
+
+    this.observable.notify(state, this.state);
+    this._state = state;
+  }
+}
+
+/**
+ * COUNTER CUBIT
+ */
+export class CounterCubit extends Cubit<CounterState> {
+  constructor(options: CubitOptions = {}) {
+    super(0, options);
+  }
+
+  increment() {
+    this.emit(this.state + 1);
+  }
+}
