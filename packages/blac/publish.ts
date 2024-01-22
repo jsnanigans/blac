5e34396fbcaae9392f583920229220a196fd388d
Brendan Mullins <brendan.mullins@9am.health>
2024-01-22 16:24:36 +0100

    config publish script

--- packages/blac/publish.ts
+++ packages/blac/publish.ts
@@ -7,7 +7,7 @@ const packageJsonConfig = {
   types: 'src/index.d.ts',
 };
 
-const filesToCopy = ['README.md', 'LICENSE', 'CHANGELOG.md', 'package.json'];
+const filesToCopy = ['README.md', '.npmignore'];
 const outputDir = 'dist';
 
 (async () => {
@@ -19,9 +19,27 @@ const outputDir = 'dist';
   }
 
   // update package.json
-  const packageJson = Bun.file(`${outputDir}/package.json`);
+  const packageJson = Bun.file(`package.json`);
   const content = await packageJson.text();
   const json = JSON.parse(content);
-  const newContent = JSON.stringify({ ...json, ...packageJsonConfig }, null, 2);
-  await Bun.write(`${outputDir}/package.json`, newContent);
+
+  // patch version
+  const currentVersion = json.version;
+  const versionParts = currentVersion.split('.');
+  versionParts[2] = Number(versionParts[2]) + 1;
+  const newVersion = versionParts.join('.');
+
+  // update package.json version
+  json.version = newVersion;
+  await Bun.write(packageJson, JSON.stringify(json, null, 2));
+
+  // write package.json in outputDir for publishing
+  const publishContent = JSON.stringify(
+    { ...json, ...packageJsonConfig },
+    null,
+    2,
+  );
+  await Bun.write(`${outputDir}/package.json`, publishContent);
+
+  console.log('Files Copied to dist and modified');
 })();
