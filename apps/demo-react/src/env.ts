60c25f5552b982df7bddde1901554b6737582a21
jsnanigans <megamaaaan@gmail.com>
2025-05-14 14:20:51 +0200

    remove demo page

--- apps/demo-react/src/env.ts
+++
@@ -1,4 +0,0 @@
-export const env = {
-    petfinder_api_key: 'xnKycTMOgyache08pmwq8yqvhL3k9jt2TmXw50HJ7RFMWPcRVn',
-    petfinder_api_secret: '4YG5GbnFWZxINSeWWYseNVFP9ao2UjbxGK7k51l8',
-}
--- apps/demo-react/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import viteReact from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
-});
