b3ca0b5d3a5145ed1d2e8fe99a9bdcf91d0a8894
jsnanigans <megamaaaan@gmail.com>
2025-05-17 12:41:31 +0200

    feat(blac): update tsconfig and vite.config for core and react packages
    - Added vite.config.ts to the include paths in tsconfig.json for both @blac/core and @blac/react packages.
    - Enhanced Vite configuration to include options for type generation and entry root.
    - Ensured consistent fileName typing in vite.config.ts for better TypeScript support.

--- packages/blac-react/vite.config.ts
+++ packages/blac-react/vite.config.ts
@@ -4,13 +4,13 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), dts({ insertTypesEntry: true })],
+  plugins: [reactRefresh(), dts({ include: ['src'], rollupTypes: true, entryRoot: 'src' })],
   publicDir: 'public',
   build: {
     lib: {
       entry: './src/index.ts', // Specifies the entry point for building the library.
       name: '@blac/react', // Sets the name of the generated library.
-      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+      fileName: (format: string) => `index.${format}.js`, // Generates the output file name based on the format.
       formats: ['cjs', 'es'], // Specifies the output formats (CommonJS and ES modules).
     },
     rollupOptions: {
--- packages/blac/vite.config.ts
+++ packages/blac/vite.config.ts
@@ -3,13 +3,21 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [
+    dts({
+      include: ['src'],
+      // Optionally, explicitly exclude tests if 'include' isn't sufficient
+      // exclude: ['tests', '**/__tests__/**', '**/*.test.ts', '**/*.spec.ts'],
+      rollupTypes: true,
+      entryRoot: 'src',
+    }),
+  ],
   publicDir: 'public',
   build: {
     lib: {
       entry: './src/index.ts', // Specifies the entry point for building the library.
       name: '@blac/core', // Sets the name of the generated library.
-      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+      fileName: (format: string) => `index.${format}.js`, // Generates the output file name based on the format.
       formats: ['cjs', 'es'], // Specifies the output formats (CommonJS and ES modules).
     },
     sourcemap: true, // Generates source maps for debugging.
