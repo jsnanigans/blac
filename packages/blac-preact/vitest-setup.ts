a19950671aefc6a7cf99b09a750b4b29a695ec51
Brendan Mullins <brendan.mullins@9am.health>
2026-05-27 14:10:41 +0200

    remove preact

--- packages/blac-preact/vitest-setup.ts
+++
@@ -1,9 +0,0 @@
-import { expect, afterEach } from 'vite-plus/test';
-import { cleanup } from '@testing-library/preact';
-import * as matchers from '@testing-library/jest-dom/matchers';
-
-expect.extend(matchers);
-
-afterEach(() => {
-  cleanup();
-});
--- packages/blac-preact/vitest.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite-plus/test/globals" />
-/// <reference types="@testing-library/jest-dom" />
-
-declare const process: {
-  env: {
-    NODE_ENV: string;
-  };
-};
